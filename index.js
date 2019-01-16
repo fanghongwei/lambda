bae0cfc71b8408eb520f1c8b225db85956afb32c
Fang Hongwei <fhw8234@gmail.com>
2019-01-16 23:12:14 +0800

    Add index.js
    [no-story]

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.text({ type: 'text/html' }))
+
+const concat = require('concat-stream');
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request -> ' + eval(req.body))
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
